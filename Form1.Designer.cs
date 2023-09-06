a85aaa39297180e708fb33168d5878b5bea31236
NIKKnak <nik88350759@yandex.ru>
2023-09-06 17:00:44 +0300

    MessageBox test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,7 +37,7 @@
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Кнопка";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
